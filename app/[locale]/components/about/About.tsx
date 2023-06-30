d0b19b6a0cb8b4847eb97f0d875a9b0915318a48
Orelbi Acosta <orelbi088@gmail.com>
2023-06-30 00:42:23 -0400

    added-hamburger-responsive-menu

--- app/[locale]/components/about/About.tsx
+++ app/[locale]/components/about/About.tsx
@@ -6,8 +6,9 @@ import Image from "next/image";
 const About = () => {
   const t = useTranslations("Index");
   return (
-    <div className="lg:grid lg:grid-cols-5 pb-[200px]">
-      <div className="hidden ml-[310px] mt-[180px] w-[400px] lg:col-span-2 lg:block">
+    <div className="flex flex-wrap lg:grid lg:grid-cols-5 pb-[200px]">
+      <div className="invisible hidden ml-[310px] mt-[180px] w-[400px] lg:col-span-2 lg:visible lg:block">
+        <div>
         <Image
           src="/puzzle/Rectangle1.png"
           width={198}
@@ -63,8 +64,9 @@ const About = () => {
           alt=""
           className="mt-[-280px] ml-[375px] h-[290px]"
         />
+        </div>
       </div>
-      <div className="flex flex-wrap mt-[40px] lg:text-right lg:mr-[400px] lg:mt-[220px] lg:col-span-3 lg:justify-end">
+      <div className="flex flex-wrap mt-[40px] lg:text-right lg:mr-[400px] lg:mt-[220px] md:col-span-3 lg:col-span-3 lg:justify-end">
         <p className="font-[700] text-[20px] text-[#3A67CC] ml-[20px]">
           Sobre nosotros
         </p>
@@ -88,64 +90,6 @@ const About = () => {
           <li className=" ">Optimización y rendimiento</li>
         </ul>
       </div>
-      {/* <div className="ml-[420px] mt-[-80px] col-span-3 mr-[300px]">
-        <p className="font-[700] text-[20px] text-[#3A67CC] text-right">
-          Sobre nosotros
-        </p>
-        <p className="font-[700] text-[36px]  text-right w-[478px] pr-[60px] pb-[30px]">
-        {t("text1")}
-        </p>
-        <p className="text-right text-[#2C2C2C] w-[414px] text-[18px] pb-[6px] font-[300px]">
-          Usted debe tener un sólido conocimiento de HTML y CSS. Debe comprender
-          la arquitectura de componentes de React. Debe tener buena atención al
-          detalle, organización del código, habilidades de comunicación efectiva
-          y capacidad para trabajar en equipo.
-        </p>
-        <p className="text-right text-[#606060] font-[700] text-[22px] pb-[36px]">
-          Usa Context para acceder a los títulos de abajo desde la sección
-          anterior
-        </p>
-        <ul>
-          <li className="flex pb-[36px] items-center flex-row-reverse text-right font-[500] text-[16px] text-[#606060] hover:text-[#3D93FF] hover:font-bold hover:underline hover:cursor-pointer">
-            <div className="h-[20px]">
-              <Image
-                src="/bullet/Bullet.png"
-                width={19}
-                height={19}
-                alt=""
-                className=""
-              />
-            </div>
-            <p>Pruebas y depuración de problemas &nbsp;</p>
-          </li>
-
-          <li className="flex  pb-[36px] items-center flex-row-reverse text-right font-[500] text-[16px] text-[#606060] hover:text-[#3D93FF] hover:font-bold hover:underline hover:cursor-pointer">
-            <div className="h-[20px]">
-              <Image
-                src="/bullet/Bullet.png"
-                width={19}
-                height={19}
-                alt=""
-                className="relative"
-              />
-            </div>
-            <p>Traduce diseños a código &nbsp;</p>
-          </li>
-
-          <li className="flex pb-[157px] items-center flex-row-reverse text-right font-[500] text-[16px] text-[#606060] hover:underline hover:text-[#3D93FF] hover:font-bold hover:cursor-pointer">
-            <div className="h-[20px]">
-              <Image
-                src="/bullet/Bullet.png"
-                width={19}
-                height={19}
-                alt=""
-                className=""
-              />
-            </div>
-            <p>Optimización y rendimiento &nbsp;</p>
-          </li>
-        </ul>
-      </div> */}
     </div>
   );
 };
--- app/[locale]/components/services/Carousel.tsx
+++ app/[locale]/components/services/Carousel.tsx
@@ -11,11 +11,6 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "./Carousel.css";
-
-import Slide1 from "../../../public/Slide1.svg";
-import Slide2 from "../../../public/Slide2.svg";
-import Slide3 from "../../../public/Slide3.svg";
-
 interface Slides {
   id: string;
   icon: string;
@@ -41,29 +36,12 @@ const Carousel = (props: any) => {
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         slidesPerView={2}
         navigation
-        // navigation={{ nextEl: ".nav-next", prevEl: ".nav-prev" }}
         pagination={{ clickable: true }}
       >
         {data!.map((slide) => {
           return (
             <SwiperSlide key={slide.id}>
-              <div className=" lg:ml-[40px] lg:mr-[100px] w-[285px] h-[400px] mt-[20px] hover:bg-[#2E53A3] pl-[30px] cursor-pointer">
-              <Image
-                  src={`/Slide${slide.id}.svg`}
-                  width={95}
-                  height={95}
-                  alt=""
-                  className="pt-[40px] group-hover:hidden"
-                  style={{ fill: "#FFFFFF" }}
-                />
-                <p className="mt-[17px] font-[600] text-[22px]">{slide.title}</p>
-                <p className="font-[400] text-[#B7B7B7] text-[14px] w-[239px] pt-[6px]">{slide.description}</p>
-                <p className="font-[600] text-[20px] pt-[5px]">
-                  Leer más
-                </p>
-              </div>
-              
-              {/* <div className=" w-[285px] h-[450px] text-black pl-[30px] hover:bg-[#2E53A3] cursor-pointer">
+              <div className="lg:ml-[40px] lg:mr-[100px] text-black w-[320px] h-[400px] mt-[20px] hover:bg-[#2E53A3] pl-[30px] cursor-pointer slide hover:text-white">
                 <Image
                   src={`/Slide${slide.id}.svg`}
                   width={95}
@@ -72,20 +50,14 @@ const Carousel = (props: any) => {
                   className="pt-[40px] group-hover:hidden"
                   style={{ fill: "#FFFFFF" }}
                 />
-
-                <p className="font-[600] text-[22px] pt-[40px]" id="title">
+                <p className="mt-[17px]  font-[600] text-[22px] card">
                   {slide.title}
                 </p>
-                <p
-                  className="font-[400] text-[#B7B7B7] text-[14px] w-[239px] pt-[6px]"
-                  id="description"
-                >
+                <p className="font-[400] text-[14px] w-[239px] pt-[6px] description card">
                   {slide.description}
                 </p>
-                <p className="font-[600] text-[20px] pt-[5px] hidden" id="more">
-                  Leer más
-                </p>
-              </div> */}
+                <p className="font-[600] text-[20px] pt-[5px] card">Leer más</p>
+              </div>
             </SwiperSlide>
           );
         })}
